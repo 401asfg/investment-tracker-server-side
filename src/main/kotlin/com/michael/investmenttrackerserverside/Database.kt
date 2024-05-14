9d105e59f71b299dac5e41261d0a78088c6bca54
michaelallan <mb.allan@ymail.com>
2024-05-14 01:19:39 -0700

    Refactored Database insert to have a single row insert helper for portfolio insert; Modified pastPrices insert to take in a list of pairs of vehicle ids and past price sets [UNFINISHED AND ERRORS]

--- src/main/kotlin/com/michael/investmenttrackerserverside/Database.kt
+++ src/main/kotlin/com/michael/investmenttrackerserverside/Database.kt
@@ -34,23 +34,9 @@ class Database(val datasource: DataSource) {
         private const val ID_COLUMN = "id"
         private const val SET_SEPARATOR = ", "
         private const val VALUE_PLACEHOLDER = "?"
-
-        /**
-         * @param items The item set to build a string representation of
-         * @param itemToString A function that converts an item into a string
-         * @return A string representation of the given item set
-         */
-        private fun <T> buildSetString(items: Set<T>, itemToString: (T) -> String): String
-            = items.fold("") { itemsString, item ->
-                "${if (itemsString == "") "" else ", "}${itemToString(item)}"
-            }
-
-        /**
-         * @param values The set of strings to build a value set string representation of
-         * @return A string representation of the given set of strings, enclosed by parentheses
-         */
-        private fun buildValuesSetString(values: Set<String>): String = "(${buildSetString(values) { item -> item }})"
     }
+
+    // FIXME: change inserts to use transactions
 
     /**
      * Inserts the given portfolio and its investments into the database
@@ -61,24 +47,18 @@ class Database(val datasource: DataSource) {
      * @throws MissingVehicleException If the portfolio's usd to base currency rate vehicle id is null
      */
     fun insert(portfolio: Portfolio) {
-        // FIXME: make a new private insert for a single row insertion?
+        val usdToBaseCurrencyRateVehicleId = portfolio.usdToBaseCurrencyRateVehicle.id
 
-        val ids = insert(
+        if (usdToBaseCurrencyRateVehicleId === null)
+            throw MissingVehicleException("Portfolio's usd to base currency rate vehicle id is null")
+
+        val id = insert(
             PORTFOLIO_TABLE,
             setOf("usd_to_base_currency_rate_vehicle_id"),
-            1
-        ) { ps: PreparedStatement, i: Int ->
-            val usdToBaseCurrencyRateVehicleId = portfolio.usdToBaseCurrencyRateVehicle.id
+            setOf(usdToBaseCurrencyRateVehicleId.toString())
+        )
 
-            if (usdToBaseCurrencyRateVehicleId === null)
-                throw MissingVehicleException("Portfolio's usd to base currency rate vehicle id is null")
-
-            ps.setInt(1, usdToBaseCurrencyRateVehicleId)
-        }
-
-        // FIXME: check that ids.size == 1
-
-        portfolio.id = ids[0]
+        portfolio.id = id
         insert(portfolio.investments, portfolio.id!!)
     }
 
@@ -129,25 +109,32 @@ class Database(val datasource: DataSource) {
             ps.setString(2, vehicles[i].name)
         }
 
+        val vehiclePastPricesPairs = mutableListOf<Pair<Int, List<PastPrice>>>()
+
         vehicles.forEachIndexed { i, vehicle ->
             vehicle.id = ids[i]
-            insert(vehicle.pastPrices, vehicle.id!!)
+            vehiclePastPricesPairs.add(Pair(vehicle.id!!, vehicle.pastPrices))
         }
+
+        insert(vehiclePastPricesPairs)
     }
 
     /**
-     * Inserts the given pastPrices into the database
+     * Inserts the past prices of each vehicle into the database
      * Assigns each past price the unique id of the database row that represents it
      *
-     * @param pastPrices The past prices to insert
-     * @param vehicleId The id of the vehicle each past price is associated with
+     * @param vehiclePastPricesPairs The past price sets that are each associated with a different vehicle id
      * @throws DataAccessException If the database was unable to perform the insertion
      */
-    fun insert(pastPrices: List<PastPrice>, vehicleId: Int) {
+    fun insert(vehiclePastPricesPairs: List<Pair<Int, List<PastPrice>>>, vehicleId: Int) {
+        val batchSize = vehiclePastPricesPairs.fold(0) { batchSize, vehiclePastPricesPair ->
+            batchSize + vehiclePastPricesPair.second.size
+        }
+
         val ids = insert(
             PAST_PRICE_TABLE,
             setOf("date_time", "price", "is_closing", "vehicle_id"),
-            pastPrices.size
+            batchSize
         ) { ps: PreparedStatement, i: Int ->
             ps.setString(1, pastPrices[i].dateTime.toTimestamp())
             ps.setFloat(2, pastPrices[i].price)
@@ -174,10 +161,8 @@ class Database(val datasource: DataSource) {
         batchSize: Int,
         setValues: (PreparedStatement, Int) -> Unit
     ): List<Int> {
-        val columnNamesString = columnNames.joinToString(SET_SEPARATOR)
-        val valuesString = columnNames.joinToString(SET_SEPARATOR) { VALUE_PLACEHOLDER }
-        val sql = "INSERT INTO $table ($columnNamesString) VALUES ($valuesString);"
-
+        val columnValues = columnNames.map { "?" }
+        val sql = buildInsertSQL(table, columnNames, columnValues.toSet())
         val keyHolder = GeneratedKeyHolder()
 
         jdbcTemplate.batchUpdate(
@@ -193,7 +178,77 @@ class Database(val datasource: DataSource) {
     }
 
     /**
-     * Queries the past prices from the database
+     * Inserts into the given database's table
+     *
+     * @param table The table to insert into
+     * @param columnNames The names of the columns to fill out
+     * @param columnValues The values to insert into each of the named columns; Must be the same size as columnNames
+     * @return The unique id of the inserted row
+     * @throws DataAccessException If the database was unable to perform the insertion
+     */
+    private fun insert(table: String, columnNames: Set<String>, columnValues: Set<String>): Int {
+        val sql = buildInsertSQL(table, columnNames, columnValues)
+        val keyHolder = GeneratedKeyHolder()
+
+        jdbcTemplate.update(
+            { con -> con.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS) },
+            keyHolder
+        )
+
+        return keyHolder.key as Int
+    }
+
+    /**
+     * @param table The table to insert into
+     * @param columnNames The names of the columns to fill out
+     * @param columnValues The values to insert into each of the named columns; Must be the same size as columnNames
+     * @return An SQL statement that inserts the given columnValues into each of the given columnNames, in the given
+     * table
+     */
+    private fun buildInsertSQL(table: String, columnNames: Set<String>, columnValues: Set<String>): String {
+        val columnNamesString = columnNames.joinToString(SET_SEPARATOR)
+        val columnValuesString = columnValues.joinToString(SET_SEPARATOR)
+        return "INSERT INTO $table ($columnNamesString) VALUES ($columnValuesString);"
+    }
+
+    // TODO: implement the rest of the queries
+    /*
+    portfolioId -> entire tree structure, using 4 join queries
+    q -> vehicles without past prices, using 1 query
+    vehicleId, fromDate, toDate, granularity -> past prices that meet specs, using 1 query
+
+    queryPortfolio(portfolioId: Int, fromDate: DateTime, toDate: DateTime, granularity: Granularity): Portfolio
+    private queryInvestments(portfolioId: Int, fromDate: DateTime, toDate: DateTime, granularity: Granularity): List<Investment>
+    private queryVehicles(portfolioId: Int, fromDate: DateTime, toDate: DateTime, granularity: Granularity): List<Vehicles>
+    private queryPastPrices(portfolioId: Int): List<PastPrice>
+
+    queryVehicles(query: String): List<Vehicle> (NO PAST PRICES)
+    queryPastPrices(vehicleId: Int, fromDate: DateTime, toDate: DateTime, granularity: Granularity): List<PastPrice>
+     */
+
+    /**
+     * Queries vehicles from the database
+     *
+     * @param ids The ids of each of the produced vehicles
+     * @return The vehicles in the database that each correspond to one of the given ids
+     * @throws DataAccessException If the database was unable to perform the insertion
+     */
+    fun queryVehicles(ids: List<Int>): List<Vehicle> {
+        val idsString = ids.joinToString(SET_SEPARATOR)
+
+        return query(VEHICLE_TABLE, "'id' IN ($idsString)") { rs, _ ->
+            val id = rs.getInt(ID_COLUMN)
+            Vehicle(
+                rs.getString("symbol"),
+                rs.getString("name"),
+                queryPastPrices(id),
+                id
+            )
+        }
+    }
+
+    /**
+     * Queries past prices from the database
      *
      * @param vehicleId The vehicle id that all produced past prices must have
      * @return All the past prices in the database that have the given vehicleId
@@ -221,177 +276,5 @@ class Database(val datasource: DataSource) {
     private fun <T> query(table: String, whereClause: String, mapRow: (ResultSet, Int) -> T): List<T> {
         val querySQL = "SELECT * FROM $table WHERE $whereClause;"
         return jdbcTemplate.query(querySQL, mapRow)
-    }
-
-    /**
-     * Queries the portfolio with the given id
-     *
-     * @param id The id of the portfolio to obtain
-     * @return The portfolio that corresponds to the given id
-     * @throws SQLException If inserting into the database fails
-     * @throws SQLTimeoutException If the database insert couldn't be performed in the allotted time
-     * @throws IllegalArgumentException If the given id doesn't correspond to any row in the portfolio table
-     * @throws SQLIntegrityConstraintViolationException If any foreign key in the portfolio or its references are
-     * invalid
-     */
-    fun queryPortfolio(id: Int): Portfolio {
-        val resultSet = executeQuery(PORTFOLIO_TABLE, id)
-        if (!resultSet.next()) throw IllegalArgumentException("Query failed to obtain any results")
-
-        val usdToBaseCurrencyRateVehicleId = resultSet.getInt("usd_to_base_currency_rate_vehicle_id")
-        val investments = queryInvestments(id)
-        val usdToBaseCurrencyRateVehicle = queryVehicle(usdToBaseCurrencyRateVehicleId)
-
-        return Portfolio(investments, usdToBaseCurrencyRateVehicle, id)
-    }
-
-    /**
-     * Queries the investments with the given portfolioId
-     *
-     * @param portfolioId The id of the portfolio that contains these investments
-     * @return The investments that have the given portfolioId
-     * @throws SQLException If inserting into the database fails
-     * @throws SQLTimeoutException If the database insert couldn't be performed in the allotted time
-     * @throws SQLIntegrityConstraintViolationException If any foreign key in the portfolio or its references are
-     * invalid
-     */
-    private fun queryInvestments(portfolioId: Int): Set<Investment> {
-        val resultSet = executeQuery(INVESTMENT_TABLE, "'portfolio_id' = $portfolioId")
-        val investments = mutableSetOf<Investment>()
-
-        while (resultSet.next()) {
-            val dateTime = DateTime(resultSet.getString("date_time"))
-            val principal = resultSet.getFloat("principal")
-            // FIXME: should query vehicles all at once, not one at a time
-            val vehicle = queryVehicle(resultSet.getInt("vehicle_id"))
-            val id = resultSet.getInt(ID_COLUMN)
-
-            val investment = Investment(dateTime, principal, vehicle, id)
-            investments.add(investment)
-        }
-
-        return investments
-    }
-
-    /**
-     * TODO: write documentation
-     */
-    private fun queryVehicle(id: Int): Vehicle {
-        // TODO: implement
-        return Vehicle("", "", setOf())
-    }
-
-    /**
-     * Executes an insert statement with the given value sets against the database
-     *
-     * @param table The name of the table to insert into
-     * @param valueSets The sets of values to insert
-     * @return The ids of the rows inserted
-     * @throws SQLException If inserting into the database fails
-     * @throws SQLTimeoutException If the database insert couldn't be performed in the allotted time
-     */
-    private fun executeInsert(table: String, valueSets: Set<Set<String>>): List<Int> {
-        val valueSetsString = buildSetString(valueSets, Database::buildValuesSetString)
-        return executeInsert(table, valueSetsString)
-    }
-
-    /**
-     * Executes an insert statement with the given values against the portfolio table of the database
-     *
-     * @param values The values to insert
-     * @return The id of the row inserted
-     * @throws SQLException If inserting into the database fails
-     * @throws SQLTimeoutException If the database insert couldn't be performed in the allotted time
-     * @throws IllegalArgumentException If inserting the given values doesn't affect exactly one row
-     */
-    private fun executePortfolioInsert(values: Set<String>): Int {
-        val valuesString = buildValuesSetString(values)
-        val ids = executeInsert(PORTFOLIO_TABLE, valuesString)
-
-        if (ids.size != 1) throw IllegalArgumentException("Insert of single set of values affected ${ids.size} rows")
-        return ids[0]
-    }
-
-    /**
-     * Executes an insert statement with the given string of value sets against the database
-     *
-     * @param table The name of the table to insert into
-     * @param valuesString The string of values to insert
-     * @return The ids of the rows inserted
-     * @throws SQLException If inserting into the database fails
-     * @throws SQLTimeoutException If the database insert couldn't be performed in the allotted time
-     */
-    private fun executeInsert(table: String, valuesString: String): List<Int>
-        = executeWrite("INSERT INTO $table VALUES $valuesString;")
-
-    /**
-     * Executes the given statement against the database
-     *
-     * @param statement The SQL write statement to execute
-     * @return The ids of the rows written
-     * @throws SQLException If inserting into the database fails
-     * @throws SQLTimeoutException If the database write couldn't be performed in the allotted time
-     */
-    private fun executeWrite(statement: String): List<Int> {
-        val instruction = connection.prepareStatement(statement, PreparedStatement.RETURN_GENERATED_KEYS)
-        val numRowsAffected = instruction.executeUpdate()
-
-        if (numRowsAffected == 0) throw SQLException("No rows were written to on executing a database write statement")
-
-        val generatedKeys = instruction.generatedKeys
-        val ids = mutableListOf<Int>()
-
-        while (generatedKeys.next()) ids.add(generatedKeys.getInt(ID_COLUMN))
-        return ids
-    }
-
-    /**
-     * Queries the given table for the row with the given id
-     *
-     * @param table The name of the table to query
-     * @param id The id of the row to obtain
-     * @return The fields of the row in the given table with the given id
-     * @throws SQLException If inserting into the database fails
-     * @throws SQLTimeoutException If the database read couldn't be performed in the allotted time
-     */
-    private fun executeQuery(table: String, id: Int): ResultSet = executeQuery(table, "'id' = $id")
-
-    /**
-     * Queries the given table for the rows with the given ids
-     *
-     * @param table The name of the table to query
-     * @param ids The ids of the rows to obtain
-     * @return The fields of the rows in the given table with the given ids
-     * @throws SQLException If inserting into the database fails
-     * @throws SQLTimeoutException If the database read couldn't be performed in the allotted time
-     */
-    private fun executeQuery(table: String, ids: Set<Int>): ResultSet {
-        val idsString = buildSetString(ids) { id -> id.toString() }
-        return executeQuery(table, "'id' IN ($idsString)")
-    }
-
-    /**
-     * Queries the given table based on the given whereClause
-     *
-     * @param table The name of the table to query
-     * @param whereClause The clause stating the conditions regarding what is queried
-     * @return The result of the query
-     * @throws SQLException If querying the database fails
-     * @throws SQLTimeoutException If the database read couldn't be performed in the allotted time
-     */
-    private fun executeQuery(table: String, whereClause: String): ResultSet
-        = executeRead("SELECT * FROM $table WHERE $whereClause;")
-
-    /**
-     * Executes the given statement against the database
-     *
-     * @param statement The SQL read statement to execute
-     * @return The result of the query
-     * @throws SQLException If reading from the database fails
-     * @throws SQLTimeoutException If the database read couldn't be performed in the allotted time
-     */
-    private fun executeRead(statement: String): ResultSet {
-        val query = connection.prepareStatement(statement)
-        return query.executeQuery()
     }
 }
